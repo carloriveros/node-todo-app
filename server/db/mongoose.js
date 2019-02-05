41d35657fbb54031fa495e8a85901861b5ed3908
Carlo <carlojuliorm@gmail.com>
2019-02-05 10:22:42 -0500

    database in mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+
+//localhost:  mongodb://localhost:27017/TodoApp
+
+mongodb://<dbuser>:<dbpassword>@ds221405.mlab.com:21405/test-todo-carlo
+mongoose.connect('mongodb://carlorm:archer37@ds221405.mlab.com:21405/test-todo-carlo');
 
 module.exports = {mongoose};
